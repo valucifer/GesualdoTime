120aa3159fddc9d25f4881719182e37d9faa4575
valucifer <v.vivone.5389@gmail.com>
2015-06-26 18:05:24 +0200

    weeee

--- New folder/resources/app/main.js
+++ New folder/resources/app/main.js
@@ -1,47 +1,29 @@
-var app = require('app');  // Module to control application life.
-var BrowserWindow = require('browser-window');  // Module to create native browser window.
-// Report crashes to our server.
+var app = require('app'); 
+var BrowserWindow = require('browser-window'); 
 require('crash-reporter').start();
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the javascript object is GCed.
 var mainWindow = null;
 
-// Quit when all windows are closed.
 app.on('window-all-closed', function() {
   if (process.platform != 'darwin')
     app.quit();
 });
-
-// This method will be called when Electron has done everything
-// initialization and ready for creating browser windows.
+app.on('window-closed', function() {
+  if (process.platform != 'darwin')
+    quit();
+});
 app.on('ready', function() {
 	var atomScreen = require('screen');
 	var size = atomScreen.getPrimaryDisplay().workAreaSize;
-  // Create the browser window.
-  mainWindow = new BrowserWindow({center:true, title:"fides", width:size.width, height:size.height});
+	
+	  mainWindow = new BrowserWindow({center:true, title:"fides", width:size.width, height:size.height,javascript:false});
+		
+	  mainWindow.loadUrl('file://' + __dirname + '/index.html');
 
-  // and load the index.html of the app.
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+	  mainWindow.openDevTools();
 
-  //Questa funzione fà apparire il controller F12
-  // Open the devtools.
-  mainWindow.openDevTools();
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
-  
-	var window = null;
-	app.on('ready', function() {
-	  window = new BrowserWindow({width: 800, height: 600});
-	  window.loadUrl('file://' + __dirname + '/index.html');
-	  window.webContents.on('did-finish-load', function() {
-		windowWebContents.print({silent: false, printBackground: false})
+	  mainWindow.on('closed', function() {
+		mainWindow = null;
 	  });
-	});
+  
 });
